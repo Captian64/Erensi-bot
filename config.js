b3182c0597abfc3c39b33cf285ab6fd62be27a76
Captian64 <118103476+Captian64@users.noreply.github.com>
2022-11-13 12:30:59 +0300

    s

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 module.exports = {
-    token: "tokeni girin (zorunlu)", 
-    prefix: "e!",
-    botdavet: "(zorunlu)",
-    desteksunucusu: "(zorunlu)",
+    token: "MTA0MTI4MTA5MTQ1MDk3ODMzNQ.Gk1ALE.9108egE-68AROr6Rgj7aqkJlYsIfnM3Ydfj73M (zorunlu)", 
+    prefix: "c!",
+    botdavet: "(https://discord.com/oauth2/authorize?client_id=1041281091450978335&scope=bot&permissions=10737429514)",
+    desteksunucusu: "(https://discord.gg/SAMvZXRZwC)",
     website: "(zorunlu)",
     topgg: "(zorunlu)",
     politika: "(zorunlu)",
